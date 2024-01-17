1b5a543d79f52f0c33b75b6a07f6781808ebd452
Victor Sales <victsales@gmail.com>
2024-01-17 15:04:57 -0300

    fix deploy script for heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,7 @@
-const URL = window.location.hostname.includes('localhost') ? 'http://localhost:8080' : 'https://victor-devflix.herokuapp.com';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
-export default{
-    URL,
+const URL = process.env.NODE_ENV !== 'production' ? 'http://localhost:8080' : API_BASE_URL;
+
+export default {
+  URL,
 };
