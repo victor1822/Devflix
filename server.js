e2276bbdefc611ec8f4735bba56b9d537b16e814
Victor Sales <victsales@gmail.com>
2024-01-17 14:44:03 -0300

    fix deploy script for heroku

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+/* eslint-disable linebreak-style */
 const jsonServer = require('json-server');
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
@@ -9,4 +11,4 @@ server.use(middlewares);
 server.use(router);
 server.listen(port, () => {
   console.log(`JSON Server is running in ${port}`);
-}); 
+});
